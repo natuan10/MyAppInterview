67beaae65ff7f38b5a1e7974c7a130530c8cdeb6
Tuấn Nguyễn <nguyenanhtuan081101@gmail.com>
2023-05-26 01:31:41 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { DatePicker, Button } from "antd";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "50px", textAlign: "center" }}>
+      <h1>Welcome to Ant Design React App</h1>
+      <DatePicker />
+      <Button type="primary" style={{ marginLeft: "10px" }}>
+        Button
+      </Button>
     </div>
   );
 }
